a266272cb16a9f1931d03cd3ad90c3a95e979a3f
Kate Hill <katelynjayne20@gmail.com>
2024-10-15 19:08:12 -0600

    main.java implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
    public static void main(String[] args) {
-      System.out.println("Testing Dockerfile");
+      System.out.println("Welcome to the calculator!\nEnter a command:");
+      Calculator calc = new Calculator();
+      Scanner scanner = new Scanner(System.in);
+      String inp = scanner.next();
+      while (!inp.equals("exit")) {
+         int num = scanner.nextInt();
+         int num2 = 0;
+         switch (inp) {
+            case "fibonacci":
+               System.out.println(calc.fibonacciNumberFinder(num));
+               break;
+            case "binary":
+               System.out.println(calc.intToBinaryNumber(num));
+               break;
+            case "add":
+               num2 = scanner.nextInt();
+               System.out.println(calc.add(num, num2));
+               break;
+            case "subtract":
+               num2 = scanner.nextInt();
+               System.out.println(calc.subtract(num, num2));
+               break;
+            case "multiply":
+               num2 = scanner.nextInt();
+               System.out.println(calc.multiply(num, num2));
+               break;
+            case "divide":
+               num2 = scanner.nextInt();
+               System.out.println(calc.divide(num, num2));
+               break;
+         }
+
+         inp = scanner.next();
+
+      }
+
    }
 }
